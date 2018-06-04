29984773c79bd9316ae6971e4afc1d8ceb1b78e4
Diogo Leite <diogo.r.leite27@gmail.com>
2018-06-04 01:47:47 -0300

    Acrescentado a pergunta sobre como voce esta

--- TesteGit/Program.cs
+++ TesteGit/Program.cs
@@ -5,6 +5,7 @@ namespace TesteGit {
         static void Main(string[] args) {
             Console.WriteLine("Olá Mundo!");
             Console.WriteLine("Bom Dia");
+            Console.WriteLine("Como você está hoje?");
             Console.ReadLine();
         }
     }
